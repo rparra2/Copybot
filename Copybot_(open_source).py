759f4ff29dd9117d76e5560d9bef6c572bee2d76
rparra2 <67341932+rparra2@users.noreply.github.com>
2020-06-25 22:10:27 -0500

    Excel fix
    The write_to_excel function supposedly works now. I'm going to commit in order to download and test.

--- Copybot_(open_source).py
+++ Copybot_(open_source).py
@@ -2,13 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-# import openpyxl
+import pandas as pd
 
-# The original purpose of this program was to retrieve all the elements with something in common of a website and
-# transfer the inner HTML over to a given excel spreadsheet. Unfortunately, I've not been able to
-# figure out how to copy them over to excel using openpyxl (which is why "import openpyxl" above is commented),
-# so for now it just prints the list in the terminal, but either way can be easily copied and pasted over.  
-# 
 # General comments about the code:
 # Everything that has "browser.find_element_by..." can be set to what best fits the HTML, usually xpath.
 # For all functions the arguments passed in must be strings. Example below 
@@ -46,24 +41,17 @@ def website_navigation(node1, node2, node3):
     wantednode3 = browser.find_element_by_xpath(node3)
     webdriver.ActionChains(browser).move_to_element(wantednode3).click(wantednode3).perform()
   
-list_of_data = []
+data = []
 def get_data(location_of_elements, attribute_wanted): 
-    datapoints = browser.find_elements_by_xpath(location_of_elements)
-    list_of_data.append(datapoints)
+    datapoints = browser.find_element_by_xpath(location_of_elements)
+    data.append(datapoints)
     for my_datapoint in datapoints:
        print(my_datapoint.get_attribute(attribute_wanted))
- 
-# Below is the failed code that is supposed to copy the list to excel.
-# If someone can figure out how to go about succesfully copying the list to excel, please contribute to this code.
-# Thank you.
-# workbook = openpyxl.load_workbook("Your path to the excel file")
-# worksheet = workbook["Name of the active sheet you plan to copy the data to"]
-# converteded_list_of_data = str(list_of_data)
-# i=2
-# for elementN in converteded_list_of_data:
-#     worksheet.cell(row=i, column=1).value = elementN
-#     i += 1
-# workbook.save(filename="How you want to save your file")
+       data.append(my_datapoint.get_attribute(attribute_wanted))
+
+def write_to_excel(name_of_column, path):
+    document = pd.DataFrame(data = data, columns = [name_of_column])
+    document.to_csv(path)
 
 
 user_login()
@@ -75,9 +63,9 @@ user_login()
 # browser.switch_to.window(browser.window_handles[0])
 website_navigation()
 # Most likely the site where you want to get data from will not have an iframe,
-# so line 81 can be commented or erased. However, the site this program was originally inteded for had an iframe
+# so line 69 can be commented or erased. However, the site this program was originally inteded for had an iframe
 # which took me, as a developer, a long time to realize and get around, so as a tribute to other new developers
-# like me, line 81 has beeen left in and commented.
+# like me, line 69 has been left in but commented.
 # browser.switch_to.frame(browser.find_element_by_tag_name("iframe"))
 get_data()
-# This is where the final function, "transfer_to_excel()" would be executed.
+write_to_excel()
